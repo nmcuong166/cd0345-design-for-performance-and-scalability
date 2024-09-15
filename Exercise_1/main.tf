172157dd02aa96f2f5525d37d084438aa63159d7
cuong nguyen <nmcuong166@gmail.com>
2024-09-16 01:30:36 +0700

    add project udacity

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,24 @@
-# TODO: Designate a cloud provider, region, and credentials
+provider "aws" {
+  region  = "us-east-1"
+  profile = "aws-udacity"
+}
 
+resource "aws_instance" "Udacity-T2" {
+  ami           = "ami-0ab4d1e9cf9a1215a"
+  instance_type = "t2.micro"
+  count         = 4
 
-# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
+  tags = {
+    Name = "Udacity T2"
+  }
+}
 
+resource "aws_instance" "Udacity-M4" {
+  ami           = "ami-0ab4d1e9cf9a1215a"
+  instance_type = "m4.large"
+  count         = 2
 
-# TODO: provision 2 m4.large EC2 instances named Udacity M4
+  tags = {
+    Name = "Udacity M4"
+  }
+}
